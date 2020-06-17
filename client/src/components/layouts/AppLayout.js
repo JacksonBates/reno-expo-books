55727e53b13b9eff8213ef169b5b85a341436c32
jacksonbates <jacksonbates@hotmail.com>
2020-06-17 21:01:42 +1000

    removes redundant imports from applayout

--- client/src/components/layouts/AppLayout.js
+++ client/src/components/layouts/AppLayout.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Layout, Menu } from "antd";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../context/auth";
