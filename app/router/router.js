d71e777d2d04d392e3fa96d2b1a709a4978af957
jacksonbates <jacksonbates@hotmail.com>
2020-06-20 21:17:14 +1000

    attempting to fix route again

--- app/router/router.js
+++ app/router/router.js
@@ -59,6 +59,10 @@ module.exports = function (app) {
   // Fallback route to render react app
 
   app.get("*", (req, res) => {
-    res.sendFile("/client/build/index.html");
+    let url = path.join(__dirname, "../client/build", "index.html");
+    if (!url.startsWith("/app/"))
+      // since we're on local windows
+      url = url.substring(1);
+    res.sendFile(url);
   });
 };
